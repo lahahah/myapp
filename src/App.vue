f4a11e6e48a5701139cbc372e41d21187b3210ef
lahahah <2454776196@qq.com>
2023-10-08 22:34:27 +0800

    修改了 Home.vue

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
 </template>
 
 <style lang="less">
-  html,body,h3{
+  html,body,h3,p{  //去掉默认样式
     margin: 0;
     padding: 0;
     overflow: hidden;  //就是溢出隐藏的意思，滚动条就隐藏了
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,24 @@
 <template>
-  <div class="home">
-    <h1>home</h1>
-  </div>
+  <el-row>
+    <el-col :span="8">
+        <el-card class="box-card">
+          <div class="user">
+            <img src="../assets/img/pic.jpg" alt=""/>
+            <div class="userinfo">
+              <p class="name">Admin</p>
+              <p class="access">超级管理员</p>
+            </div>
+          </div>
+
+        </el-card>
+    </el-col>
+
+    <el-col :span="16">
+
+        内容
+
+    </el-col>
+  </el-row>
 </template>
 
 <script>
@@ -12,3 +29,37 @@ export default {
   name: 'HomeView',
 }
 </script>
+
+<style lang="less" scoped>
+.el-row {
+  margin-bottom: 20px;
+  &:last-child {
+    margin-bottom: 0;
+  }
+}
+.el-col {
+  border-radius: 4px;
+}
+
+
+.user{
+  display: flex;
+  align-items: center;  //纵轴垂直居中
+  img{
+    width: 100px;
+    height: 100px;
+    border-radius: 50%;
+    margin-right: 40px;
+  }
+}
+
+.userinfo{
+  .name{
+    font-size: 32px;
+    margin-bottom: 10px;
+  }
+  .access{
+    color: #999999;
+  }
+}
+</style>
