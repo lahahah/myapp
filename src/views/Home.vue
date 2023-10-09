d1c3a259f6da1a4e9ad5328ad46ac12a4cc19ac1
lahahah <2454776196@qq.com>
2023-10-09 18:26:33 +0800

    再次提交 Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,9 @@
 <template>
+<!-- 行 -->
   <el-row>
-
+<!-- 左侧列 -->
     <el-col :span="8">
+      <!--   头像   -->
         <el-card class="box-card">
       <!--分割线(下边框)上面-->
           <div class="user">
@@ -17,16 +19,56 @@
               <p>上次登录时间：</p><p>2023.10.10</p>
             </div>
             <div class="info">
-              <p>上次登录地点：</p><p>上海</p>
+              <p>上次登录地点：</p><p>上海</p>  <!-- <span></span> 就可以在一行显示 -->
             </div>
           </div>
         </el-card>
+      <!-- 表格，使用循环表头  -->
+      <el-card style="margin-top: 20px; height: 460px;">
+<!--        <el-table
+            :data="tableData"
+            stripe
+            style="width: 100%">
+          <el-table-column
+              prop="name"
+              label="课程"
+              >
+          </el-table-column>
+          <el-table-column
+              prop="todayBuy"
+              label="今日购买"
+              >
+          </el-table-column>
+          <el-table-column
+              prop="monthBuy"
+              label="本月购买">
+          </el-table-column>
+          <el-table-column
+              prop="totalBuy"
+              label="总购买">
+          </el-table-column>
+        </el-table>-->
+
+        <el-table :data="tableData" stripe style="width: 100%">
+         <!--  值是以键值对格式存储  -->
+          <el-table-column v-for="(value,key,index) in tableLabel" :key="index" :prop="key" :label="value">
+
+          </el-table-column>
+        </el-table>
+      </el-card>
     </el-col>
 
+    <!-- 右侧列 -->
     <el-col :span="16">
-
-        内容
-
+      <div class="num">
+        <el-card v-for="data in countData" :key="data.name" :body-style="{display: 'flex'}">
+          <i class="icon" :class="`el-icon-${data.icon}`" :style="{background: data.color}"></i>
+          <div class="detail">
+            <p class="price">￥{{  data.value }}</p>
+            <p class="desc">{{ data.name }}</p>
+          </div>
+        </el-card>
+      </div>
     </el-col>
 
   </el-row>
@@ -38,6 +80,92 @@
 
 export default {
   name: 'HomeView',
+  data(){
+    return{
+      tableData: [
+        {
+          name: 'oppo',
+          todayBuy: 100,
+          monthBuy: 300,
+          totalBuy: 800
+        },
+        {
+          name: 'vivo',
+          todayBuy: 100,
+          monthBuy: 300,
+          totalBuy: 800
+        },
+        {
+          name: '苹果',
+          todayBuy: 100,
+          monthBuy: 300,
+          totalBuy: 800
+        },
+        {
+          name: '小米',
+          todayBuy: 100,
+          monthBuy: 300,
+          totalBuy: 800
+        },
+        {
+          name: '三星',
+          todayBuy: 100,
+          monthBuy: 300,
+          totalBuy: 800
+        },
+        {
+          name: '魅族',
+          todayBuy: 100,
+          monthBuy: 300,
+          totalBuy: 800
+        }
+      ],
+      tableLabel: {
+        name: "课程",
+        todayBuy: "今日购买",
+        monthBuy: "本月购买",
+        totalBuy: "总购买",
+      },
+      countData: [
+        {
+          name: "今日支付订单",
+          value: 1234,
+          icon: "success",
+          color: "#2ec7c9",
+        },
+        {
+          name: "今日收藏订单",
+          value: 210,
+          icon: "star-on",
+          color: "#ffb980",
+        },
+        {
+          name: "今日未支付订单",
+          value: 1234,
+          icon: "s-goods",
+          color: "#5ab1ef",
+        },
+        {
+          name: "本月支付订单",
+          value: 1234,
+          icon: "success",
+          color: "#2ec7c9",
+        },
+        {
+          name: "本月收藏订单",
+          value: 210,
+          icon: "star-on",
+          color: "#ffb980",
+        },
+        {
+          name: "本月未支付订单",
+          value: 1234,
+          icon: "s-goods",
+          color: "#5ab1ef",
+        },
+      ],
+    }
+  }
 }
 </script>
 
@@ -88,6 +216,43 @@ export default {
     p{
       margin-right: 20px;
     }
+  }
+}
+
+.num{
+  display: flex;
+  flex-wrap:wrap;
+  justify-content: space-between;
+  .icon{
+    width: 80px;
+    height: 80px;
+    font-size: 30px;
+    color: #fff;
+    text-align: center;
+    line-height: 80px;
+  }
+  .detail{
+    margin-left: 15px;
+    display: flex;
+    flex-direction: column;  //改变主轴为上下显示
+    justify-content: center;  //控制项目在主轴上居中对齐
+    align-items: flex-start;
+    .price{
+      font-size: 20px;
+      font-weight: bold;
+      margin-bottom: 10px;
+      line-height: 30px;
+      height: 30px;
+    }
+    .desc{
+      font-size: 10px;
+      color: #999999;
+      text-align: center;
+    }
+  }
+  .el-card{
+    width: 32%;
+    margin-bottom: 20px;
   }
 }
 </style>
